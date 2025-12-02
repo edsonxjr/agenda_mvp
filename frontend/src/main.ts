1c85204a6ce15fdf42a0e69af2eebea6e976314a
Edson Junior <108370882+edsonxjr@users.noreply.github.com>
2025-12-02 13:11:23 -0300

    att

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App) 
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+app.use(router)            
+
+app.mount('#app')         
