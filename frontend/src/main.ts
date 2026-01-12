6e085e1e8b89ee5f16d63bb65c6b8ae4bfe83448
Edson Junior <108370882+edsonxjr@users.noreply.github.com>
2026-01-12 09:40:54 -0300

    att

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
+import { vMaska } from "maska/vue" 
+
 const app = createApp(App)
+
+app.directive("maska", vMaska) 
 
 app.use(router)
 app.mount('#app')
